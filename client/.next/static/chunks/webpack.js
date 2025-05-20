99587f0d66c8be26b7f060d38ae39957bf99f403
Lucas Silva <lrdspc@gmail.com>
2025-05-20 15:18:56 -0300

    gitt

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "67cfd0fd37d2a469"; }
+/******/ 		__webpack_require__.h = function() { return "bb64396f0f87eaec"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
